5f4bf7ca3a6d3dbf8c3f516f23260e560719db7f
G Jake Gebbie <ggebbie@whoi.edu>
2021-03-02 20:47:29 -0500

    completed tests on 4 A matrices

--- tmi_diagnostics.m
+++ tmi_diagnostics.m
@@ -32,7 +32,7 @@ addpath(datadir)
 TMIproducts = {'GH2010_2x2deg','GH2010_4x4deg','G2012_4x4deg','G2014_4x4deg'}
 
 % choose the case
-TMIno = 2; % select 1, 2, 3, 4
+TMIno = 1; % select 1, 2, 3, 4
 TMIversion = TMIproducts{TMIno}
 
 switch TMIversion
@@ -130,6 +130,10 @@ end
  
  lon_section = 330;
  isec = find(LON==330);
+ if isempty(isec) 
+     isec = find(LON==329);
+ end
+     
  figure(101)
  contourf(LAT,-DEPTH,squeeze(C(:,:,isec)),0:0.05:1) % a sample plot at 22 W.
 
@@ -146,9 +150,12 @@ end
  % to get quantity of dyed water throughout ocean:
  DP = Q * (U \ (L \ (P * (R \ d))));  
  DPfld = vector_to_field(DP,i,j,k);  % g is the 3-d field of water-mass concentration.
- 
+
  lon_section = 330;
  isec = find(LON==330);
+ if isempty(isec) 
+     isec = find(LON==329);
+ end
  figure(102)
  contourf(LAT,-DEPTH,squeeze(DPfld(:,:,isec)),0:0.05:1) % a sample plot at 22 W.
  
@@ -215,7 +222,7 @@ end
  NZ = max(k);
  NY = max(j);
  NX = max(i);
- Nfield = size(A,1);
+ N = size(A,1);
  
  % choose an interior location X (Xlon[lon], Xlat [lat], Xdepth [m depth]).
 % -7.38N, 115.26E
@@ -254,295 +261,5 @@ end
  figure(104)
  contourf(LON,LAT,Vtot,[0 exp(-9:.5:-5)]) % quick plot
 
- 
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Example 4: Find the distribution of a tracer given:              %
-%       (a) the pathways described by A,                          %
-%       (b) interior sources and sinks given by dC,               % 
-%           that best fits observations, Cobs,                    %
-%   and (c) inequality constraints on the tracer concentration.   %
-%                                                                  %
-% Mathematically, minimize J = (C-Cobs)^T W (C-Cobs) subject to    %
-%                         AC = d + Gamma u                         %
-%  where u is the estimated change in surface concentration.    % 
-%
-% See Supplementary Section 2, Gebbie & Huybers 2011.
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
- 
-  % use temperature as an example, with Tobs the observed temperature vector.
-  % load temperature in vector form, along with associated error.
-  % choose version of tracer observations
-dataname = {'tracerobs_2deg_33lev_woce.mat','tracerobs_4deg_33lev_woce.mat','tracerobs_4deg_33lev_TMI.mat'}
-datano = 2; % choose 1,2, or 3
-if exist(dataname{datano})
-    load(dataname{datano})
-else
-    disp('not an available choice')
-end
-
-% check that the choice is consistent with pathways matrix.
-if length(Tobs) ~= N
-    disp('inconsistent obs and pathways matrix')
-end
-
-dT = Tobs;      % set rhs vector to the observations.
-dT(k>1) = 0;    % no internal sinks or sources.
-
-% a first guess: observed surface boundary conditions are perfect.  
-Tmod1 =  Q * (U \ (L \ (P * (R \ dT))));
-Tmod1_field = vector_to_field(Tmod1,i,j,k);
- 
-% get cost function (J) for first guess temperature field.
-% here the data-model misfit is weighted by the expected error. 
-% N = length(Tobs);
-invWT{1} = sparse(1:N,1:N,1./Terr.^2,N,N); % diagonal matrix.
-J1 = (Tmod1-Tobs)'*invWT{1}*(Tmod1-Tobs)./N % optimized J1 = 1
- 
-% Get better fit to observations by modifying the surface boundary 
-% condition within its uncertainty.
-% mathematical form: Find uT such that:
-% J = (Tmod-Tobs)'*inv(WT)*(Tmod-Tobs) is minimized 
-% subject to:  A*Tmod = dT + Gamma * uT.
-% Nsfc = sum(k==1);
-Gamma = sparse(N,Nsfc);
-nu = 0;
-for nv = 1:N
-    if k(nv)==1
-        nu = nu+1;
-        Gamma(nv,nu) = 1;
-    end
-end
-u0 = zeros(Nsfc,1); % first guess of change to surface boundary
-                      % conditions.
-lbT = -2.*ones(Nsfc,1); % temperature lower bound: can not freeze.
-ubT = 40.*ones(Nsfc,1); % ad-hoc temperature upper bound: 40 C.
-
-%% choose a method below 
-
-method = 1;
-if method==1
-    % 1) quadratic programming using full Hessian
-    H1 = Q * (U \ (L \ (P * (R \ dT)))) -Tobs; 
-    H2 = Q * (U \ (L \ (P * (R \ Gamma))));   % lengthy calculation step: get full
-                                              % Hessian. Can be avoided by specifying
-                                              % functional form of Hessian-vector product.
-
-    tmp1 = invWT{1}*H2;
-    HT = H2'*tmp1;
-    fT = 2.*H1'*tmp1;
-    uT = quadprog(HT,fT,[],[],[],[],lbT,[],u0); % solve quadratic
-                                                % programming problem for
-                                                % shifts (uT) to
-                                                % sfc. temp.
-    J1 = objfun(uTtest,A,invWT,Tobs,isfc,inotmixlyr,noncons) %
-                                       
-    d1 = zeros(N,1); d1(isfc) = uT;
-    Tmod1 =  Q * (U \ (L \ (P * (R \ d2)))) ; % best estimate
-    
-elseif method==3 % slower because doesn't use Hessian.
-      %%  3) Constrained minimization (without inequality constraints),
-      
-      maxiters = 10; % increase for a better fit.
-      options = optimset('Algorithm','interior-point','Display','iter', ...
-                         'GradObj','on','LargeScale','on','maxiter',maxiters);
-      %options = optimset('Algorithm','trust-region-reflective','Display','iter', ...
-      %        'GradObj','on','LargeScale','on');
-      %isfc = find(kt==1);
-
-      noncons = 0;
-      uTtest= fmincon(@(x)objfun(x,A,invWT,Tobs,isfc,inotmixlyr,noncons),Tobs(isfc),[],[],[],[],lbT,ubT,[],options);
-      J2 = objfun(uTtest,A,invWT,Tobs,isfc,inotmixlyr,noncons) %
-                                                         % expect J2 ~ 1, J2<J1
-      d2 = zeros(N,1); d2(isfc) = uTtest;
-      Tmod2 =  Q * (U \ (L \ (P * (R \ d2)))) ; % best estimate
-end
-% Note: other methods are: 3) quadratic programming using functional form of Hessian, 
-% 4) Unconstrained minimization. 
-  
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Example 4B: Same as example 4 but the observations are sparse and
-% the inequality constraint is dropped.
-%
-%        Find the distribution of a tracer given:                 %
-%       (a) the pathways described by A,                          %
-%       (b) interior sources and sinks given by dC,               % 
-%           that best fits THE SPARSE observations, Cobs,         %
-%                                                                  %
-% Mathematically, minimize J = (C-Cobs)^T W (C-Cobs) subject to    %
-%                         AC = d + Gamma u                         %
-%  where u is the estimated change in surface concentration.    % 
-%
-% See Supplementary Section 2, Gebbie & Huybers 2011.
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
-% load a circulation field.
-% load A_4deg_2012
-
-% get the longitude, latitude, depth, value of the
-% observations, and their expected error.
-% (lon_obs, lat_obs, depth_obs, y, err_obs)
-lon_obs = [160 180 200 220];
-lat_obs = [0   0   0   0 ];
-depth_obs=[2e3 2e3 2e3 2e3];
-
-
-LONv = LON(i); % grid longitudes in vector form.
-LATv = LAT(j);
-E = Emaker(lon_obs,lat_obs,depth_obs,LONv,LATv,k,DEPTH,6);
-
-% For this example, sample the observations perfectly from existing
-% temperature observations.
-load tracerobs_4deg_33lev_woce Tobs Terr
-y = E*Tobs;
-err_obs = 0.1;
-
-W  = 1./err_obs.^2;
-
-% set up the control variables.
-Nsfc = sum(k==1);
-isfc = find(k==1);
-N = length(k);
-Gamma = sparse(isfc,1:Nsfc,ones(Nsfc,1),N,Nsfc);
-
-% what is the uncertainty in the surface boundary condition?
-err_d = 1;
-diagonal = false; % or set to false.
-if diagonal 
-  S     = 1./err_d.^2; 
-else
-  % if diagonal==false
-  % impose spatial smoothing in surface b.c.
-  lengthscale = 4; % horizontal lengthscale in units of gridcells
-  factor = 0.126.*(1/lengthscale)^2 ; 
-  load Del2_4deg.mat
-  S = factor.* sparse(1:Nsfc,1:Nsfc,1./err_d.^2);
-  S = S + Del2'*(lengthscale^4.*S*Del2);
-end
-  
-% Get the first guess of surface boundary conditions and interior
-% sources and sinks.
-% d= first guess of r.h.s.
-d0 = Tobs;   % truth is imposed as first-guess boundary condition.
-d0(k>1) = 0; % conservative in interior
-
-% c0 = first guess property field
-c0 = A\d0;
-
-% r = first guess misfit.
-r = E*c0-y;
-
-% get transpose(f)
-f1 =   W*(E'*r);
-f2 =  A'\f1;
-f  = Gamma'*f2;
-clear fT1 fT2
-
-% 2 methods to solve: iterative or direct
-iterative = false;
-if iterative % method 1
-  % put in the form of a least-squares problem. Hu = -f;
-  u = lsqr(@(x,tflag)afun(x,Gamma,A,E,W,S),-f,1e-6,20);
-else % method 2
-  tic; H = get_hessian(Gamma,A,E,W,S); toc;
-  u = -H\f;
-end
-
-% update the boundary conditions with the least squares solution.
-d = d0 + Gamma*u;
-
-% solve for the three dimensional property field.
-c = A\d;
-cfld = vector_to_field(c,i,j,k);
-
-% how much did the data points reduce the error (globally).
-sqrt(sum((c-Tobs).^2)/N)
-sqrt(sum((c0-Tobs).^2)/N)
-
-% how much did the data points reduce the error (at data points).
-Nobs = length(y);
-sqrt(sum((E*c-y).^2)/Nobs)
-sqrt(sum((E*c0-y).^2)/Nobs)
-  
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Example 4C: Same as example 4B but the tracer is nonconservative.
-%
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
-% load a circulation field.
-% load A_4deg_2012
-
-% get the longitude, latitude, depth, value of the
-% observations, and their expected error.
-% (lon_obs, lat_obs, depth_obs, y, err_obs)
-lon_obs = [160 180 200 220];
-lat_obs = [0   0   0   0 ];
-depth_obs=[2e3 2e3 2e3 2e3];
-
-LONv = LON(i); % grid longitudes in vector form.
-LATv = LAT(j);
-E = Emaker(lon_obs,lat_obs,depth_obs,LONv,LATv,k,DEPTH,6);
-
-% For this example, sample the observations perfectly from existing
-% temperature observations.
-load tracerobs_4deg_33lev_woce Pobs 
-y = E*Pobs;
-err_obs = 0.1;
-
-W  = 1./err_obs.^2;
-
-% set up the control variables.
-
-% what is the uncertainty in the first guess global field?
-% for sake of argument, use the global observations as the first
-% guess.
-c0 = 1.0.*ones(N,1);
-err_c0 = 3;
-S     = 1./err_d.^2; % this step could be done better -- impose
-                     % spatial smoothing in surface b.c.
-
-% Get the first guess of surface boundary conditions and interior
-% sources and sinks.
-% d= first guess of r.h.s.
-
-% diagnose the nonconservative effects in the dataset.
-% surface boundary conditions.
-q = A*Pobs;
-
-% to be consistent: use same surface boundary condition from above.
-
-% what is error in surface boundary conditions?
-err_sfc = 5;
-
-% what is the assumed error in the nonconservative effects?
-err_noncons = 1e-3;
-
-% turn into a weighting matrix.
-qerr = zeros(N,1);
-qerr(k==1) = err_sfc;
-qerr(k>1) = err_noncons;
-Q = sparse(1:N,1:N,1./qerr.^2);
-
-ETWE = E'*(W*E);
-ATQA = A'*(Q*A);
-
-H = ETWE+ATQA+S*speye(N);
-
-f = E'*(W*y) + A'*(Q*q) + S.*(c0);
-
-c = H\f;
-
-% update the boundary conditions with the least squares solution.
-cfld = vector_to_field(c,i,j,k);
-
-% how much did the data points reduce the error (globally).
-sqrt(sum((c-Pobs).^2)/N)
-sqrt(sum((c0-Pobs).^2)/N)
-
-% how much did the data points reduce the error (at data points).
-Nobs = length(y);
-sqrt(sum((E*c-y).^2)/Nobs)
-sqrt(sum((E*c0-y).^2)/Nobs)
-
-% unlike Example 4, does not enforce non-negativity.
 
    
