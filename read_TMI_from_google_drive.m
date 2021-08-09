55de73645593358dde485b02376c0acb84392e50
G Jake Gebbie <ggebbie@whoi.edu>
2021-08-09 17:06:57 -0400

    Use ! commands in read_TMI_from_google_drive
    Helps the script work when called from somewhere else in the path.

--- read_TMI_from_google_drive.m
+++ read_TMI_from_google_drive.m
@@ -2,4 +2,14 @@
 
 % Or, download manually at: https://docs.google.com/uc?export=download&id=1L5i5eQ0QCrrqPKGoAxuB8X4_CltvQBMD and https://docs.google.com/uc?export=download&id=1xAkrTNybqoAKtFMuJ9XU9z9KZwnLenzm
 
-! /bin/sh read_TMI_from_google_drive.sh
+% Download TMI_4x4deg_data.tar.gz from Google Drive
+!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L5i5eQ0QCrrqPKGoAxuB8X4_CltvQBMD' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L5i5eQ0QCrrqPKGoAxuB8X4_CltvQBMD" -O TMI_4x4deg_data.tar.gz && rm -rf /tmp/cookies.txt
+
+% Download TMI_2x2deg_data.tar.gz from Google Drive
+! wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xAkrTNybqoAKtFMuJ9XU9z9KZwnLenzm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xAkrTNybqoAKtFMuJ9XU9z9KZwnLenzm" -O TMI_2x2deg_data.tar.gz && rm -rf /tmp/cookies.txt
+
+!tar xvzf TMI_2x2deg_data.tar.gz 
+!rm TMI_2x2deg_data.tar.gz
+
+!tar xvzf TMI_4x4deg_data.tar.gz 
+!rm TMI_4x4deg_data.tar.gz
